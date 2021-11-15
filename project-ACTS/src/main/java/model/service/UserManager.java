26b0f4d39df97390b545499a8f8f123ac8b23380
오윤아 <yuna9927@naver.com>
2021-11-15 18:07:54 +0900

    211115 수정

--- project-ACTS/src/main/java/model/service/UserManager.java
+++ project-ACTS/src/main/java/model/service/UserManager.java
@@ -3,9 +3,7 @@ package model.service;
 import java.sql.SQLException;
 import java.util.List;
 
-import model.Community;
 import model.User;
-import model.dao.CommunityDAO;
 import model.dao.UserDAO;
 
 /**
@@ -18,14 +16,10 @@ import model.dao.UserDAO;
 public class UserManager {
 	private static UserManager userMan = new UserManager();
 	private UserDAO userDAO;
-	private CommunityDAO commDAO;
-	private UserAnalysis userAanlysis;
 
 	private UserManager() {
 		try {
 			userDAO = new UserDAO();
-			commDAO = new CommunityDAO();
-			userAanlysis = new UserAnalysis(userDAO);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}			
@@ -44,25 +38,11 @@ public class UserManager {
 
 	public int update(User user) throws SQLException, UserNotFoundException {
 		int oldCommId = findUser(user.getUserId()).getCommId();
-		if (user.getCommId() != oldCommId) { 	// 소속 커뮤티니가 변경됨
-			Community comm = commDAO.findCommunity(oldCommId);  // 기존 소속 커뮤니티
-			if (comm != null && user.getUserId().equals(comm.getChairId())) {
-				// 사용자가 기존 소속 커뮤니티의 회장인 경우 -> 그 커뮤니티의 회장을 null로 변경 및 저장
-				comm.setChairId(null);
-				commDAO.updateChair(comm);
-			}
-		}
 		return userDAO.update(user);
 	}	
 
 	public int remove(String userId) throws SQLException, UserNotFoundException {
 		int commId = findUser(userId).getCommId();
-		Community comm = commDAO.findCommunity(commId);  // 소속 커뮤니티
-		if (comm != null && userId.equals(comm.getChairId())) {
-			// 사용자가 소속 커뮤니티의 회장인 경우 -> 그 커뮤니티의 회장을 null로 변경 및 저장
-			comm.setChairId(null);
-			commDAO.updateChair(comm);
-		}
 		return userDAO.remove(userId);
 	}
 
@@ -93,37 +73,6 @@ public class UserManager {
 			throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
 		}
 		return true;
-	}
-
-	public List<User> makeFriends(String userId) throws Exception {
-		return userAanlysis.recommendFriends(userId);
-	}
-	
-	public Community createCommunity(Community comm) throws SQLException {
-		return commDAO.create(comm);		
-	}
-
-	public int updateCommunity(Community comm) throws SQLException {
-		return commDAO.update(comm);				
-	}
-	
-	public Community findCommunity(int commId) throws SQLException {
-		Community comm = commDAO.findCommunity(commId); 
-		
-		List<User> memberList = userDAO.findUsersInCommunity(commId);
-		comm.setMemberList(memberList);
-		
-		int numOfMembers = userDAO.getNumberOfUsersInCommunity(commId);
-		comm.setNumOfMembers(numOfMembers);
-		return comm;
-	}
-	
-	public List<Community> findCommunityList() throws SQLException {
-		return commDAO.findCommunityList();
-	}
-	
-	public List<User> findCommunityMembers(int commId) throws SQLException {
-		return userDAO.findUsersInCommunity(commId);
 	}
 
 	public UserDAO getUserDAO() {
