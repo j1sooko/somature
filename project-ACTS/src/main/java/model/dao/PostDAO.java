8cd86c130080f2846f958aff73984a9352ebc369
hello <hello@DESKTOP-20TDTIA>
2021-11-21 10:28:20 +0900

    postDAO: title 검색하는 dao 알고리즘 작업함. 경원~

--- project-ACTS/src/main/java/model/dao/PostDAO.java
+++ project-ACTS/src/main/java/model/dao/PostDAO.java
@@ -187,26 +187,5 @@ private JDBCUtil jdbcUtil = null;
 		
 		return null;
 	}
-	
-//	/**
-//	 * 二쇱뼱吏� �궗�슜�옄 ID�뿉 �빐�떦�븯�뒗 �궗�슜�옄媛� 議댁옱�븯�뒗吏� 寃��궗 
-//	 */
-//	public boolean existingUser(String accountId) throws SQLException {
-//		String sql = "SELECT count(*) FROM ACCOUNT WHERE accountId=?";      
-//		jdbcUtil.setSqlAndParameters(sql, new Object[] {accountId});	// JDBCUtil�뿉 query臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙
-//
-//		try {
-//			ResultSet rs = jdbcUtil.executeQuery();		// query �떎�뻾
-//			if (rs.next()) {
-//				int count = rs.getInt(1);
-//				return (count == 1 ? true : false);
-//			}
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//		} finally {
-//			jdbcUtil.close();		// resource 諛섑솚
-//		}
-//		return false;
-//	}
 
 }
