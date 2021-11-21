4c84c23775b3a90c4758918b8d68a8208163a6dd
hello <hello@DESKTOP-20TDTIA>
2021-11-21 10:23:07 +0900

    postDAO searchPostList 알고리즘 작업

--- project-ACTS/src/main/java/model/dao/PostDAO.java
+++ project-ACTS/src/main/java/model/dao/PostDAO.java
@@ -8,39 +8,39 @@ import java.util.List;
 import model.Post;
 
 /**
- * 게시글 관리를 위해 데이터베이스 작업을 전담하는 DAO 클래스
- * POST 테이블에 사용자 정보를 추가, 수정, 삭제, 검색 수행 
+ * 寃뚯떆湲� 愿�由щ�� �쐞�빐 �뜲�씠�꽣踰좎씠�뒪 �옉�뾽�쓣 �쟾�떞�븯�뒗 DAO �겢�옒�뒪
+ * POST �뀒�씠釉붿뿉 �궗�슜�옄 �젙蹂대�� 異붽�, �닔�젙, �궘�젣, 寃��깋 �닔�뻾 
  */
 public class PostDAO {
 private JDBCUtil jdbcUtil = null;
 	
 	public PostDAO() {			
-		jdbcUtil = new JDBCUtil();	// JDBCUtil 객체 생성
+		jdbcUtil = new JDBCUtil();	// JDBCUtil 媛앹껜 �깮�꽦
 	}
 		
 	/**
-	 * 게시글 관리 테이블에 새로운 게시글 생성.
+	 * 寃뚯떆湲� 愿�由� �뀒�씠釉붿뿉 �깉濡쒖슫 寃뚯떆湲� �깮�꽦.
 	 */
 	public int create(Post post) throws SQLException {
 		String sql = "INSERT INTO POST VALUES (id_seq.nextval, ?, ?, ?, DEFAULT, ?, ?, ?, ?, ?, ?)";		
 		Object[] param = new Object[] { post.getTitle(), post.getDesc(), post.getImgUrl(), post.getCategoryId(), 
 				post.getViews(), post.getStatus(), post.getPrice(), post.getpType(), post.getWriterId()};		
-		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정	
+		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil �뿉 insert臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙	
 		try {
-			int result = jdbcUtil.executeUpdate();	// insert 문 실행
+			int result = jdbcUtil.executeUpdate();	// insert 臾� �떎�뻾
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {	 
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
+			jdbcUtil.close();	// resource 諛섑솚
 		}
 		return 0;			
 	}
 
 	/**
-	 * 기존의 게시글 정보를 수정.
+	 * 湲곗〈�쓽 寃뚯떆湲� �젙蹂대�� �닔�젙.
 	 */
 	public int update(Post post) throws SQLException {
 		String sql = "UPDATE POST "
@@ -48,10 +48,10 @@ private JDBCUtil jdbcUtil = null;
 					+ "WHERE postId=?";
 		Object[] param = new Object[]  { post.getTitle(), post.getDesc(), post.getImgUrl(), post.getCategoryId(), 
 				post.getViews(), post.getStatus(), post.getPrice() };			
-		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
+		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil�뿉 update臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙
 			
 		try {				
-			int result = jdbcUtil.executeUpdate();	// update 문 실행
+			int result = jdbcUtil.executeUpdate();	// update 臾� �떎�뻾
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
@@ -59,20 +59,20 @@ private JDBCUtil jdbcUtil = null;
 		}
 		finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
+			jdbcUtil.close();	// resource 諛섑솚
 		}		
 		return 0;
 	}
 
 	/**
-	 * 게시글 ID에 해당하는 게시글을 삭제.
+	 * 寃뚯떆湲� ID�뿉 �빐�떦�븯�뒗 寃뚯떆湲��쓣 �궘�젣.
 	 */
 	public int remove(String postId) throws SQLException {
 		String sql = "DELETE FROM POST WHERE postId=?";		
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {postId});	// JDBCUtil에 delete문과 매개 변수 설정
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {postId});	// JDBCUtil�뿉 delete臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙
 
 		try {				
-			int result = jdbcUtil.executeUpdate();	// delete 문 실행
+			int result = jdbcUtil.executeUpdate();	// delete 臾� �떎�뻾
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
@@ -80,26 +80,26 @@ private JDBCUtil jdbcUtil = null;
 		}
 		finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
+			jdbcUtil.close();	// resource 諛섑솚
 		}		
 		return 0;
 	}
 
 	/**
-	 * 주어진 게시글 ID에 해당하는 게시글 정보를 데이터베이스에서 찾아 Post 도메인 클래스에 
-	 * 저장하여 반환.
+	 * 二쇱뼱吏� 寃뚯떆湲� ID�뿉 �빐�떦�븯�뒗 寃뚯떆湲� �젙蹂대�� �뜲�씠�꽣踰좎씠�뒪�뿉�꽌 李얠븘 Post �룄硫붿씤 �겢�옒�뒪�뿉 
+	 * ���옣�븯�뿬 諛섑솚.
 	 */
 	public Post findPost(int postId) throws SQLException {
         String sql = "SELECT title, description, imageUrl, createdTime, categoryId, "
         			+ "views, status, price, postType, writerId "
         			+ "FROM POST "
         			+ "WHERE postId=? ";              
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {postId});	// JDBCUtil에 query문과 매개 변수 설정
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {postId});	// JDBCUtil�뿉 query臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙
 
 		try {
-			ResultSet rs = jdbcUtil.executeQuery();		// query 실행
-			if (rs.next()) {						// 학생 정보 발견
-				Post post = new Post(		// User 객체를 생성하여 학생 정보를 저장
+			ResultSet rs = jdbcUtil.executeQuery();		// query �떎�뻾
+			if (rs.next()) {						// �븰�깮 �젙蹂� 諛쒓껄
+				Post post = new Post(		// User 媛앹껜瑜� �깮�꽦�븯�뿬 �븰�깮 �젙蹂대�� ���옣
 					postId,
 					rs.getString("title"),
 					rs.getString("description"),
@@ -116,25 +116,25 @@ private JDBCUtil jdbcUtil = null;
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
-			jdbcUtil.close();		// resource 반환
+			jdbcUtil.close();		// resource 諛섑솚
 		}
 		return null;
 	}
 
 	/**
-	 * 전체 게시글 정보를 검색하여 List에 저장 및 반환
+	 * �쟾泥� 寃뚯떆湲� �젙蹂대�� 寃��깋�븯�뿬 List�뿉 ���옣 諛� 諛섑솚
 	 */
 	public List<Post> findPostList() throws SQLException {
         String sql = "SELECT title, views, status, price, postType, writerId " 
         		   + "FROM POST "
         		   + "ORDER BY postId";
-		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil에 query문 설정
+		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil�뿉 query臾� �꽕�젙
 					
 		try {
-			ResultSet rs = jdbcUtil.executeQuery();			// query 실행			
-			List<Post> postList = new ArrayList<Post>();	// User들의 리스트 생성
+			ResultSet rs = jdbcUtil.executeQuery();			// query �떎�뻾			
+			List<Post> postList = new ArrayList<Post>();	// User�뱾�쓽 由ъ뒪�듃 �깮�꽦
 			while (rs.next()) {
-				Post post = new Post(			// User 객체를 생성하여 현재 행의 정보를 저장
+				Post post = new Post(			// User 媛앹껜瑜� �깮�꽦�븯�뿬 �쁽�옱 �뻾�쓽 �젙蹂대�� ���옣
 						rs.getInt("postId"),
 						rs.getString("title"),
 						rs.getInt("views"),
@@ -142,27 +142,61 @@ private JDBCUtil jdbcUtil = null;
 						rs.getInt("price"),
 						rs.getString("postType"),
 						rs.getInt("writerId"));
-				postList.add(post);				// List에 User 객체 저장
+				postList.add(post);				// List�뿉 User 媛앹껜 ���옣
 			}		
 			return postList;					
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
-			jdbcUtil.close();		// resource 반환
+			jdbcUtil.close();		// resource 諛섑솚
 		}
 		return null;
 	}
 	
+	public List<Post> SearchPostList(String title) throws SQLException {
+		String sql = "SELECT postId "
+				+ "FROM POST "
+				+ "WHERE title LIKE '%"
+				+ title
+				+ "%'";
+		jdbcUtil.setSqlAndParameters(sql, null);
+		
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();
+			List<Post> postList = new ArrayList<Post>();
+			
+			while(rs.next()) {
+				Post post = new Post(
+						rs.getInt("postId"),
+						rs.getString("title"),
+						rs.getInt("views"),
+						rs.getString("status"),
+						rs.getInt("price"),
+						rs.getString("postType"),
+						rs.getInt("writerId"));
+				postList.add(post);	
+			
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		
+		return null;
+	}
+	
 //	/**
-//	 * 주어진 사용자 ID에 해당하는 사용자가 존재하는지 검사 
+//	 * 二쇱뼱吏� �궗�슜�옄 ID�뿉 �빐�떦�븯�뒗 �궗�슜�옄媛� 議댁옱�븯�뒗吏� 寃��궗 
 //	 */
 //	public boolean existingUser(String accountId) throws SQLException {
 //		String sql = "SELECT count(*) FROM ACCOUNT WHERE accountId=?";      
-//		jdbcUtil.setSqlAndParameters(sql, new Object[] {accountId});	// JDBCUtil에 query문과 매개 변수 설정
+//		jdbcUtil.setSqlAndParameters(sql, new Object[] {accountId});	// JDBCUtil�뿉 query臾멸낵 留ㅺ컻 蹂��닔 �꽕�젙
 //
 //		try {
-//			ResultSet rs = jdbcUtil.executeQuery();		// query 실행
+//			ResultSet rs = jdbcUtil.executeQuery();		// query �떎�뻾
 //			if (rs.next()) {
 //				int count = rs.getInt(1);
 //				return (count == 1 ? true : false);
@@ -170,7 +204,7 @@ private JDBCUtil jdbcUtil = null;
 //		} catch (Exception ex) {
 //			ex.printStackTrace();
 //		} finally {
-//			jdbcUtil.close();		// resource 반환
+//			jdbcUtil.close();		// resource 諛섑솚
 //		}
 //		return false;
 //	}
