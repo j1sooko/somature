cfac5c2d59ee74bb73ee10c8c4647cf37786b8c0
snowm <snowman4453@gmail.com>
2021-11-21 13:42:15 +0900

    Create issue31 postList, searchForm, UploadPostController.java

--- project-ACTS/src/main/java/controller/post/UploadPostController.java
+++ project-ACTS/src/main/java/controller/post/UploadPostController.java
@@ -6,50 +6,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.Controller;
-import controller.user.UserSessionUtils;
 import model.Post;
-import model.User;
-import model.service.*;
+import model.service.PostManager;
 //import model.service.UserManager;
 public class UploadPostController implements Controller{
 	private static final Logger log = LoggerFactory.getLogger(UploadPostController.class);
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		String curUserId = UserSessionUtils.getLoginUserId(request.getSession());
-		UserManager userManager = UserManager.getInstance();
-		User user = userManager.findUser(curUserId);
-		System.out.println("user: " + user);
 		Post prod = new Post(
+				Integer.parseInt(request.getParameter("postId")),
 				request.getParameter("title"),
-				request.getParameter("description"),
 				request.getParameter("status"),
 				Integer.parseInt(request.getParameter("price")),
-				request.getParameter("pType"),
-				user.getUserId()
+				request.getParameter("description"),
+				request.getParameter("accountId"),
+				Integer.parseInt(request.getParameter("curUserId"))
 				);
-		
-		log.debug("Create ProductForm : {}", prod);
-		
 		try {
-
-			PostManager postManager = PostManager.getInstance();
-			postManager.create(prod);
-
+			PostManager manager = PostManager.getInstance();
+			manager.create(prod);
 			
 			log.debug("Create ProductForm : {}", prod);
-	        return "redirect:/comm/main";
+	        return "foward:/comm/main";
 		}
 		catch(Exception e) {
 			request.setAttribute("uploadFail", true);
 			request.setAttribute("exception", e);
 			request.setAttribute("post", prod);
-			//게시글 작성 화면으로 이동
-			return "/post/postForm.jsp";
+			return "foward:/post/uploadFail";
 		}
-
-	
-
 	}
 	
 }
