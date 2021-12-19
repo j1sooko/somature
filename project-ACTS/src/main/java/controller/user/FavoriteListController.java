c7575588436ef78c74686ba03bf11638ac5e2ba5
hello <hello@DESKTOP-20TDTIA>
2021-12-19 23:23:09 +0900

    마이페이지에서 내가 팔로잉하는계정 나타나게하기

--- project-ACTS/src/main/java/controller/user/FavoriteListController.java
+++ project-ACTS/src/main/java/controller/user/FavoriteListController.java
@@ -1,32 +1,56 @@
 package controller.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import controller.Controller;
+import model.service.FavoriteManager;
+import model.service.PostManager;
 import model.service.UserManager;
 import model.service.UserNotFoundException;
+import model.Favorite;
+import model.Post;
 import model.User;
 
 public class FavoriteListController implements Controller {
 
+	@SuppressWarnings("null")
 	@Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {			
     	
     	
 		UserManager manager = UserManager.getInstance();
+		FavoriteManager fm = FavoriteManager.getInstance();
+		PostManager pm = PostManager.getInstance();
+		
 		HttpSession session = request.getSession();
 		String loginAccountId = UserSessionUtils.getLoginUserId(session);
-
+		List<Favorite> favoriteList = new ArrayList<Favorite>();
+		List<Post> postList = new ArrayList<Post>();
     	User user = null;
+    	
+    	
     	try {
 			user = manager.findUser(loginAccountId);	// 사용자 정보 검색
+			favoriteList = fm.findFavoriteListByUserId(user.getUserId());
+			
 		} catch (UserNotFoundException e) {				
-	        return "redirect:/user/login";
-		}	
+	        return "redirect:/user/myPage";
+		}
+    	
+    	for(Favorite data: favoriteList) {
+			int postId = data.getPostId();
+			System.out.println(postId);
+			Post post = pm.findPost(postId);
+			System.out.println(post);
+			postList.add(post);
+		}
 		
-    	request.setAttribute("user", user);		// 사용자 정보 저장				
+    	request.setAttribute("postList", postList);		// 사용자 정보 저장				
 		return "/user/favoriteList.jsp";				// 사용자 보기 화면으로 이동
     }
 
